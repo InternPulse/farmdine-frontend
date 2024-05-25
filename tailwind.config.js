932475a61cbdba24ac91853b0914051c3bad64f8
acubeos <80309463+acubeos@users.noreply.github.com>
2024-05-26 00:23:55 +0100

    Build HomeSection Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				green0: "#8BC34A",
+				green47: "#658E36",
+				green100: "#425D23",
+			},
+		},
+	},
+	plugins: [],
 }
-
