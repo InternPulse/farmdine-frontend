0849c78451140cec7f3e4129c6abe29a6368fcc5
Racheline <136965489+Rachel2001@users.noreply.github.com>
2024-05-26 23:55:24 +0200

    created navbar, did not add function as yet, now have to push it to main branch to be reviewed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+// src/App.js
 import React from "react";
-import Navbar from "./Navbar/Navbar";
-import "./Navbar/Navbar.css";
+import "./App.css"; 
+import navLogo from "./images/nav-logo.jpg";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Navbar />
+    <div className="container">
+      <nav className="navbar">
+        <img src={navLogo} alt="Company Logo" className="logo" />
+        <div className="products">
+          <a href="#" className="nav-link-home">
+            Home
+          </a>
+          <a href="#" className="nav-link">
+            Product Listing
+          </a>
+          <a href="#" className="nav-link">
+            Contact
+          </a>
+          <a href="#" className="nav-link">
+            Help
+          </a>
+        </div>
+        <a href="#" className="log-in-button">
+          Log in
+        </a>
+        <a href="#" className="sign-up-button">
+          Sign Up
+        </a>
+      </nav>
     </div>
   );
 }
+
+export default App;
+
