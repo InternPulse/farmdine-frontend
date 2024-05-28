fdbcd17c6169e8459a91f0994f6c207de621d95e
tramoonyane <theteleramoonyane5@gmail.com>
2024-05-29 00:08:25 +0200

    Completed implementation of Product Section feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import SignUpSelectionPage from "./pages/SignUpSelectionPage"
-=======
-import ContactPage from "./pages/ContactPage"
-import HomeSection from "./pages/HomeSection"
-// import Dashboard from "./pages/Dashboard"
-import SignUpPage from "./pages/SignUpPage"
->>>>>>> 71a849bdbc05f547d64e03529a7f462c3bfa18d0
+
+import React from 'react';
+import ProductSection from './components/ProductSection';
 
 const App: React.FC = () => {
-	return (
-		<>
-<<<<<<< HEAD
-			<SignUpSelectionPage />
-=======
-			<HomeSection />
-			<ContactPage />
-			{/* <Dashboard />  */}
-			<SignUpPage />
->>>>>>> 71a849bdbc05f547d64e03529a7f462c3bfa18d0
-		</>
-	)
+    return (
+        <div>
+            <ProductSection />
+        </div>
+    );
 }
 
-export default App
+export default App;
