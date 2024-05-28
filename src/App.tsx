b8306ddf03b64eccb2af7b74c6a855ace32711d9
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-05-28 12:54:48 +0200

    Added functionality to the dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+
 import HomeSection from "./Components/HomeSection"
+// import Dashboard from "./pages/Dashboard"
+import SignUpPage from "./pages/SignUpPage"
+
 
 const App: React.FC = () => {
 	return (
 		<>
 			<HomeSection />
+
+			{/* <Dashboard />  */}
+			<SignUpPage />
+
 		</>
 	)
 }
