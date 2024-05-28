46f0b34c0adbff9e14e4a5d74951a954f9260b97
Anezijane <ahnnieanezi@gmail.com>
2024-05-28 13:03:16 +0100

    add background to events

--- src/Components/Events.tsx
+++ src/Components/Events.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-// Sample data for the carousel
+// data for the carousel
 const eventData = [
   { id: 1, title: "Event 1", description: "World Farmers' Day, also known as National Farmers' Day, is celebrated on December 23rd every year. It is a day to recognize and appreciate the contributions of farmers to our society. Farmers play a vital role in feeding our communities, preserving natural resources, and supporting local economies." },
   { id: 2, title: "Event 2", description: "Description of Event 2" },
@@ -22,7 +22,7 @@ const Events: React.FC = (): React.ReactElement => {
     <div>
       <h1 className="text-3xl font-bold text-center">Events</h1>
       <p className="text-center mt-4">Farming events unite the agricultural community to celebrate, share, and innovate.</p>
-      <div className="relative w-full max-w-lg mx-auto mt-8 overflow-hidden">
+      <div className="relative w-full max-w-lg mx-auto mt-8 overflow-hidden bg-slate-500 ">
         <div className="text-center p-6 transition-transform duration-500 ease-in-out transform">
           <h2 className="text-2xl font-semibold">{eventData[currentIndex].title}</h2>
           <p className="mt-2">{eventData[currentIndex].description}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+      }
+    },
   },
   plugins: [],
 }
