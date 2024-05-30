b9afec9e248b8cdf408a32ad00227548bc70aaf7
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-05-30 20:03:08 +0200

    Contact Form for contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TeamMembers from '../components/contactPage/TeamMembers'
+import ContactForm from '../components/contactPage/ContactForm'
 
 const ContactPage: React.FC = () => {
   return (
     <div>
         <TeamMembers />
+        <ContactForm />
     </div>
   )
 }
