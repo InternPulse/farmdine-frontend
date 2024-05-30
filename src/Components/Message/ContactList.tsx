2a89839f4d10e9d507b82ac786e1af0ec9eb5585
Anezijane <ahnnieanezi@gmail.com>
2024-05-30 13:39:40 +0100

    add input icon and add styles

--- src/Components/Message/ContactList.tsx
+++ src/Components/Message/ContactList.tsx
@@ -8,10 +8,11 @@ interface ContactListProps {
 
 const ContactList: React.FC<ContactListProps> = ({ contacts, onSelectContact }) => {
   return (
-    <div className="w-1/4 border-r border-gray-300 mt-8 pl-8">
+    <div className="w-1/4 border-r mt-8 pl-8">
       <h1 className='font-bold text-xl pb-3'>Messages</h1>
-      <div>
-        <input type="search" name="search" id="search" placeholder='search here...' />
+      <div className='flex bg-slate-200 p-1 w-60 rounded-lg'>
+        <img className='m-1 w-4 h-4 bg-slate-200' src="src/assets/search-icon.png" alt="search icon" />
+        <input className='text-sm pl-2 border-none outline-0 bg-slate-200' type="search" name="search" id="search" placeholder='search here...' />
       </div>
       {contacts.map(contact => (
         <ContactItem
