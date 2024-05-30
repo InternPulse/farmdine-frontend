c82e2685d24591125bcec41df2ef423e877921d1
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-05-30 13:33:31 +0200

    Removed duplicate dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import HomeSection from "./pages/HomeSection"
+// import Dashboard from "./pages/Dashboard"
+// import SignUpPage from "./pages/SignUpPage"
+import Events from "./components/Events"
 
 const App: React.FC = () => {
 	return (
 		<>
-			
+		<Navbar />
+			<HomeSection />
+            <Events />
 		</>
 	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  assetsInclude: ["**/*.png", "**/*.jpg"],
 });
