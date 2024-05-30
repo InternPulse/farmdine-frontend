69f8b93c325e9867ba2b453277d6d3a856598b9d
Anezijane <ahnnieanezi@gmail.com>
2024-05-30 11:01:59 +0100

    add chat window content

---
+++ src/Components/Message/ChatWindow.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ChatMessage from './ChatMessage';
+
+interface ChatWindowProps {
+  messages: { id: string; text: string; sender: string; }[];
+}
+
+const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
+  return (
+    <div className="w-3/4 h-full p-4 flex flex-col">
+      <div className="flex-grow overflow-y-auto">
+        {messages.map(message => (
+          <ChatMessage key={message.id} message={message} />
+        ))}
+      </div>
+      <div className="border-t border-gray-300 p-4">
+        <input type="text" className="w-full p-2 border rounded" placeholder="Type a message" />
+      </div>
+    </div>
+  );
+};
+
+export default ChatWindow;
