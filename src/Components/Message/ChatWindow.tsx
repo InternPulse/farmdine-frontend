cd01a76a5ed69cdaf15741ec7cff850f53139188
Anezijane <ahnnieanezi@gmail.com>
2024-05-30 22:59:03 +0100

    add profile bar icons

--- src/Components/Message/ChatWindow.tsx
+++ src/Components/Message/ChatWindow.tsx
@@ -8,17 +8,17 @@ interface ChatWindowProps {
 const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
   return (
     <div className="w-3/4 h-full p-4 flex flex-col bg-slate-100">
-        <div className='flex bg-slate-50 py-3 pl-2 rounded'>
-            <div className='flex'>
-               <img className='w-9 h-9' src="src/assets/profile1.jpg" alt="sender profile picture" />
-                <p>Ruth Godfrey</p> 
-            </div>
-            <div>
-                <img src="" alt=""/>
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </div>
-        </div>
+      <div className='flex justify-between items-center bg-slate-50 py-3 pl-2 rounded'>
+          <div className='flex'>
+              <img className='w-9 h-9' src="src/assets/profile1.jpg" alt="sender profile picture" />
+              <p>Ruth Godfrey</p> 
+          </div>
+          <div className='flex'>
+              <img className='w-4 h-4' src="src/assets/search-icon.png" alt="search icon"/>
+              <img className='w-4 h-4' src="src/assets/call.jpg" alt="call icon" />
+              <img className='w-4 h-4' src="src/assets/more-circle.jpg" alt="icon more" />
+          </div>
+      </div>
       <div className="flex-grow overflow-y-auto">
         {messages.map(message => (
           <ChatMessage key={message.id} message={message} />
