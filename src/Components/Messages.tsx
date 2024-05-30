cce250b2b570c63e5be84fb27a4d5e7456d7e32b
Anezijane <ahnnieanezi@gmail.com>
2024-05-30 15:03:50 +0100

    change individual chat background

--- src/Components/Messages.tsx
+++ src/Components/Messages.tsx
@@ -3,31 +3,31 @@ import ContactList from './Message/ContactList';
 import ChatWindow from './Message/ChatWindow';
 
 const contacts = [
-  { id: '1', name: 'Aisha Stephanie' },
-  { id: '2', name: 'Ogbeide Ayoola' },
-  { id: '3', name: 'Micheal Angelo' },
-  { id: '4', name: 'Aziz Akande' },
-  { id: '5', name: 'Reacheline Moyana' },
-  { id: '6', name: 'Thetele Ramoonyane' },
-  { id: '7', name: 'Anezi Jane' },
-  { id: '8', name: 'Aisha Musa' },
-  { id: '9', name: 'Muhammad Musa' },
-  { id: '10', name: 'Ruth Godfrey' },
-  { id: '11', name: 'Blessing Joseph' },
+  { id: 1, imageUrl: 'src/assets/Frame605.jpg', name: 'Aisha Stephanie', message: "Hi there, I saw your product..." },
+  { id: 2, imageUrl: 'src/assets/Frame605.jpg', name: 'Ogbeide Ayoola', message: "Hi there, I saw your product..." },
+  { id: 3, imageUrl: 'src/assets/Frame605.jpg', name: 'Micheal Angelo', message: "Hi there, I saw your product..." },
+  { id: 4, imageUrl: 'src/assets/Frame605.jpg', name: 'Aziz Akande', message: "Hi there, I saw your product..." },
+  { id: 5, imageUrl: 'src/assets/Frame605.jpg', name: 'Reacheline Moyana', message: "Hi there, I saw your product..." },
+  { id: 6, imageUrl: 'src/assets/Frame605.jpg', name: 'Thetele Ramoonyane', message: "Hi there, I saw your product..."  },
+  { id: 7, imageUrl: 'src/assets/Frame605.jpg', name: 'Anezi Jane', message: "Hi there, I saw your product..."  },
+  { id: 8, imageUrl: 'src/assets/Frame605.jpg', name: 'Aisha Musa', message: "Hi there, I saw your product..."  },
+  { id: 9, imageUrl: 'src/assets/Frame605.jpg', name: 'Muhammad Musa' , message: "Hi there, I saw your product..." },
+  { id: 10,imageUrl: 'src/assets/Frame605.jpg',  name: 'Ruth Godfrey', message: "Hi there, I saw your product..."  },
+  { id: 11,imageUrl: 'src/assets/Frame605.jpg',  name: 'Blessing Joseph', message: "Hi there, I saw your product..."  },
 ];
 
 const messages = {
-  '1': [
-    { id: '1', text: 'Hello Alice!', sender: 'me' },
-    { id: '2', text: 'Hi there!', sender: 'Alice' },
+  1: [
+    { id: 1, text: 'Thank you for getting back to me. I am interested in purchasing a basket of fruit. Could you please provide more details on its specifications, pricing, and availability? Additionally, I would like to know about the payment options and delivery timeline.', sender: 'me' },
+    { id: 2, text: 'Thank you for getting back to me. I’m interested in purchasing a basket of fruit. Could you please provide more details on its specifications.', sender: 'Alice' },
   ],
-  '2': [
-    { id: '1', text: 'Hey Bob!', sender: 'me' },
-    { id: '2', text: 'Hello!', sender: 'Bob' },
+  2: [
+    { id: 1, text: 'Hey Bob!', sender: 'me' },
+    { id: 2, text: 'Hello!', sender: 'Bob' },
   ],
-  '3': [
-    { id: '1', text: 'Hi Charlie!', sender: 'me' },
-    { id: '2', text: 'Hey!', sender: 'Charlie' },
+  3: [
+    { id: 1, text: 'Hi Charlie!', sender: 'me' },
+    { id: 2, text: 'Hey!', sender: 'Charlie' },
   ],
 };
 
