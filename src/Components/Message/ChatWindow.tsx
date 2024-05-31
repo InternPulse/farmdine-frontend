9bfef03836beda6bad1824dd22727c76ab9b9845
Anezijane <ahnnieanezi@gmail.com>
2024-05-31 09:58:35 +0100

    add send icon,update styles

--- src/Components/Message/ChatWindow.tsx
+++ src/Components/Message/ChatWindow.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ChatMessage from './ChatMessage';
 
 interface ChatWindowProps {
@@ -10,13 +9,13 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
     <div className="w-3/4 h-full p-4 flex flex-col bg-slate-100">
       <div className='flex justify-between items-center bg-slate-50 py-3 pl-2 rounded'>
           <div className='flex ml-10 space-x-6'>
-              <img className='w-9 h-9' src="src/assets/profile1.jpg" alt="sender profile picture" />
-              <p className='mt-2'>Ruth Godfrey</p> 
+              <img className='profile-image w-9 h-9' src="src/assets/profile1.jpg" alt="sender profile picture" />
+              <p className='mt-2 profile-name'>Aisha Stephanie</p> 
           </div>
           <div className='flex mr-10 space-x-6'>
               <img className='w-4 h-4' src="src/assets/search-icon.png" alt="search icon"/>
               <img className='w-4 h-4' src="src/assets/call.jpg" alt="call icon" />
-              <img className='w-4 h-4' src="src/assets/more-circle.jpg" alt="icon more" />
+              <img className='w-4 h-4' src="src/assets/more-circle.jpg" alt="Open options icon" />
           </div>
       </div>
       <div className="flex-grow overflow-y-auto">
@@ -24,8 +23,12 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ messages }) => {
           <ChatMessage key={message.id} message={message} />
         ))}
       </div>
-      <div className="border-t border-gray-300 p-4">
-        <input type="text" className="w-full p-2 border rounded" placeholder="Type a message" />
+      <div className="border-t border-gray-300 p-2 flex">
+        <div>
+          <input type="text" className=" mr-2 rounded pl-4 py-1" placeholder="Type your messages.." />
+          <img className='w-4 h-4' src="src/assets/more-circle.jpg" alt="Open options icon" />
+        </div>
+        <img className='w-10 h-8' src="src/assets/send-icon.jpg" alt="send icon" />
       </div>
     </div>
   );
