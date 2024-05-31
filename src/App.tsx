862c691e7f0b45a994421d4c82f854dc179dedf0
acubeos <80309463+acubeos@users.noreply.github.com>
2024-05-31 23:15:40 +0100

    Build Home Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import HomeSection from "./components/HomeSection"
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold bg-yellow-900 text-white">
-      Farmdine
-    </h1>
-  )
+	return (
+		<>
+			<HomeSection />
+		</>
+	)
 }
