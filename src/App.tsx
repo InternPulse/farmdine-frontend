3cb1a93ccf3a97266ad14ee045801d27f182634c
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-05-31 10:43:47 +0200

    Subscribe form updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/HomeSection";
+import ProductListing from "./pages/ProductListing";
+import Contact from "./pages/ContactPage";
+import Help from "./pages/Help";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUpPage";
+import Footer from "./components/Footer";
+
 const App: React.FC = () => {
-	return <></>
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TeamMembers from '../components/contactPage/TeamMembers'
 import ContactForm from '../components/contactPage/ContactForm'
+import Subscribe from '../components/contactPage/Subscribe'
 
 const ContactPage: React.FC = () => {
   return (
     <div>
         <TeamMembers />
         <ContactForm />
+        <Subscribe />
     </div>
   )
 }
