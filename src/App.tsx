d40b5fe4bfcb3d2e3e168372b690b99eacbc22a9
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-06-01 13:28:01 +0200

    Typescript installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import SignUpPage from './pages/SignUpPage'
 import SignUpSelectionPage from './pages/SignUpSelectionPage'
 
 const App: React.FC = () => {
-	  return (
-	<div>
-	  <h1 className="text-4xl text-center mt-8">FarmDine</h1>
-	  <SignUpPage />
-	  <SignUpSelectionPage />
+	return (
+		<div>
+			<h1 className="text-4xl text-center mt-8">FarmDine</h1>
+			<SignUpPage />
+			<SignUpSelectionPage />
 	</div>
   )
 }
