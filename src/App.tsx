301d94cffc122b0897bf83875b86eec86415642f
acubeos <80309463+acubeos@users.noreply.github.com>
2024-06-01 12:49:08 +0100

    Repo Resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import HomeSection from "./components/HomeSection"
 import Navbar from "./components/Navbar"
+import Product from "./pages/Product"
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
+			<HomeSection />
+			<Product />
 		</>
 	)
 }
