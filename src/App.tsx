d5ab46c884ac58c28787da196b11b7f2b7e2b2a3
tramoonyane <theteleramoonyane5@gmail.com>
2024-06-01 02:45:23 +0200

    merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-import ProductSection from './pages/ProductSection';
 
-export default function App() {
-	return (
-		<>
-			<ProductSection />
-		</>
-	)
-=======
-export default function App() {
-	return <></>
->>>>>>> 8f7696608add5f8f3ff8b695b227a6d51d7b8c2e
-}
