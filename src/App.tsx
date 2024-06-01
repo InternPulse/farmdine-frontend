8185141a5f395772b4f5387dee56a65702f5fddc
tramoonyane <theteleramoonyane5@gmail.com>
2024-06-01 03:01:06 +0200

    why this conflicts when i pull from dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import ProductSection from './pages/ProductSection';
 
+const App: React.FC = () => {
+    return (
+        <div>
+            <ProductSection />
+        </div>
+    );
+}
+
+export default App;
