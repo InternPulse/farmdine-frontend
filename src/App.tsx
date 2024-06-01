b5a835b044b235d7c82caa56d4dc995a469b52f3
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-06-01 13:19:45 +0200

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import SignUpPage from './pages/SignUpPage'
+import SignUpSelectionPage from './pages/SignUpSelectionPage'
 
 const App: React.FC = () => {
 	  return (
 	<div>
 	  <h1 className="text-4xl text-center mt-8">FarmDine</h1>
-	  <
+	  <SignUpPage />
+	  <SignUpSelectionPage />
 	</div>
   )
 }
