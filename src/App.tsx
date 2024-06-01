75b954b129d0d298745813069324668bac2cd827
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-06-01 13:18:13 +0200

    Simple app.tsx test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <></>;
+import React from 'react'
+
+const App: React.FC = () => {
+	  return (
+	<div>
+	  <h1 className="text-4xl text-center mt-8">FarmDine</h1>
+	  <
+	</div>
+  )
 }
+
+export default App
