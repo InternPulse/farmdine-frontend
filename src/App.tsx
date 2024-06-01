e38009390a9ea69b1bef2385ce3585cf86130477
acubeos <80309463+acubeos@users.noreply.github.com>
2024-06-01 21:34:49 +0100

    Build product listing section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import HomeSection from "./components/HomeSection"
 import Navbar from "./components/Navbar"
+import ProductSection from "./components/ProductSection"
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<HomeSection />
+			<ProductSection />
 		</>
 	)
 }
