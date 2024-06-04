acdb4fe219decad3e7c69110bd5e5ba78783f846
Anezijane <ahnnieanezi@gmail.com>
2024-06-05 00:30:00 +0100

    add checkout content structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Navbar from "./components/Navbar"
 import ProductSection from "./components/ProductSection"
 import Subscribe from "./components/Subscribe"
 import Events from "./components/Events"
+import CheckOut from "./components/CheckOut"
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<CheckOut />
 			<HomeSection />
 			<ProductSection />
 			<Events />
--- src/components/CheckOut.tsx
+++ src/components/CheckOut.tsx
@@ -3,7 +3,21 @@
 const CheckOut = () => {
   return (
     <div>
-      
+        <h1><img src="" alt="left arrow"/> Checkout</h1>
+        <div>
+            <p>Payment Details</p>
+            <div>
+                <img src="" alt="Google pay logo"/>
+                <img src="" alt="Visa pay logo"/>
+                <img src="" alt="Master card pay logo"/>
+            </div>
+        </div>
+        <form action="#">
+            <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" name="email" id="email" />
+            </div>
+        </form>
     </div>
   )
 }
