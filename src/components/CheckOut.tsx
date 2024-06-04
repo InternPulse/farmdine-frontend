a3461777f0f008f089a8bdcc236cf1311c6d222e
Anezijane <ahnnieanezi@gmail.com>
2024-06-05 00:45:29 +0100

    add checkout content structure content

--- src/components/CheckOut.tsx
+++ src/components/CheckOut.tsx
@@ -15,8 +15,40 @@ const CheckOut = () => {
         <form action="#">
             <div>
                 <label htmlFor="email">Email Address</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" placeholder="aishastepahnie12@gmail.com"/>
             </div>
+            <div>
+                <label htmlFor="name">Name on Card</label>
+                <input type="text" name="name" id="name" placeholder="Aisha Stephanie" />
+            </div>
+            <div>
+                <label htmlFor="card-number">Card Number</label>
+                <div><img src="" alt=""/><input type="number" name="card-number" id="card-number" /></div>
+            </div>
+            <div>
+                <div>
+                    <label htmlFor="date">Expire</label>
+                    <input type="date" name="date" id="date" placeholder="05/2028" />
+                </div>
+                <div>
+                    <label htmlFor="number">CVV</label>
+                    <input type="number" name="number" id="number" placeholder="***" />
+                </div>
+            </div>
+            <div>
+                <p>Shipping</p>
+                <p>Free</p>
+            </div>
+            <div>
+                <p>Subtotal</p>
+                <p>$50.00</p>
+            </div>
+            <hr />
+            <div>
+                <p>Total</p>
+                <p>$50.00</p>
+            </div>
+            <button type="button">Continue to Checkout</button>
         </form>
     </div>
   )
