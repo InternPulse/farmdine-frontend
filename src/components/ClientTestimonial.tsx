16777755cc08a339021b78dd5c5e090b76e68d13
nemisholaye <nemisholaye@gmail.com>
2024-06-07 17:53:39 +0100

    Add client testimonial component

---
+++ src/components/ClientTestimonial.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+
+const TestimonialSection: React.FC = () => {
+  return (
+    <div className="container flex justify-center items-center w-full h-full px-[191px] py-[48px] flex-col gap-10">
+      <section className="testimonial-section py-16 bg-white flex flex-col items-center gap-">
+        <div className="flex justify-center items-center w-[252.283px] p-[11.957px] gap-[11.957px] mx-auto bg-lime-custom rounded-[38.261px]">
+          <p className="text-center text-[20px] font-openSans font-semibold text-green0">
+            Client Testimonials
+          </p>
+        </div>
+        <div className="text-center text-[40px] font-roboto font-bold text-black mt-[21.522px]">
+          What clients say about us
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 testimonials-container">
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-bold mb-4"
+                style={{
+                  fontFamily: "Roboto",
+                  fontSize: "20px",
+                  color: "#1E1E1E",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Tunde Badmus
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                FarmDine has transformed my farm. Now, I can reach buyers who
+                value locally grown, organic produce. The platform showcases my
+                authenticity, ensuring steady sales and supporting sustainable
+                farming.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-roboto font-medium text-base mb-4"
+                style={{
+                  color: "#1E1E1E",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Chioma wheeler
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                FarmDine catapulted my homemade food business to new heights.
+                With their platform, I expanded distribution while preserving
+                recipe integrity. A game-changer for passionate small producers
+                like me.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-roboto font-medium text-base mb-4"
+                style={{
+                  color: "#1E1E1E",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Aisha Ngozi
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                Timely supply and affordable prices, It is good to do
+                businessThanks to FarmDine, ingredient sourcing for my
+                restaurant is effortless. No more endless searches or quality
+                concerns.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-roboto font-medium text-base mb-4"
+                style={{
+                  color: "#1E1E1E",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Tunde Badmus
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                FarmDine has transformed my farm. Now, I can reach buyers who
+                value locally grown, organic produce. The platform showcases my
+                authenticity, ensuring steady sales and supporting sustainable
+                farming.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-roboto font-medium text-base mb-4"
+                style={{
+                  color: "#1E1E1E",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Chioma wheeler
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                FarmDine catapulted my homemade food business to new heights.
+                With their platform, I expanded distribution while preserving
+                recipe integrity. A game-changer for passionate small producers
+                like me.
+              </p>
+            </div>
+          </div>
+
+          <div className="p-testimonials bg-testimonialsBg rounded-testimonials shadow-testimonials backdrop-blur-testimonials h-auto">
+            <div className="flex flex-col justify-center">
+              <h3
+                className="text-gray-600 font-roboto font-medium text-base mb-4"
+                style={{
+                  color: "#1E1E1E",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "23.913px",
+                }}
+              >
+                Aisha Ngozi
+              </h3>
+              <p
+                className="text-gray-800 text-lg mb-4"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#1E1E1E",
+                  fontFamily: "Open Sans",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                }}
+              >
+                Timely supply and affordable prices, It is good to do
+                businessThanks to FarmDine, ingredient sourcing for my
+                restaurant is effortless. No more endless searches or quality
+                concerns.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default TestimonialSection;
