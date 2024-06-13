69085f2afae63b56800acc1e3b4105abc738af51
Anezijane <ahnnieanezi@gmail.com>
2024-06-13 01:41:24 +0100

    add config for animation

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -5,7 +5,7 @@ const HomeSection = () => {
 	return (
 		<div className='container mx-auto pb-9'>
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-20 px-24'>
-				<div className='pt-64'>
+				<div className='pt-64 left-slide-in animate__animated animate__fadeInLeft'>
 					<h1 className='text-4xl font-bold'>
 						<span className='bg-gradient-to-r from-lime-300 via-lime-500 to-lime-700 inline-block text-transparent bg-clip-text pr-2'>
 							FarmDine
@@ -18,7 +18,7 @@ const HomeSection = () => {
 						Get started <img src={arrow} alt='' className='w-6 h-6' />
 					</button>
 				</div>
-				<div>
+				<div className="right-slide-in animate__animated animate__fadeInRight">
 					<img src={homeSection} alt='homeSection Image' className='mt-40' />
 				</div>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ export default {
 				'eventbg': "url('public/images/eventsbg.jpg')",
 				'subscribebg': "url('public/images/subscribebg.png')"
 			},
+			keyframes: {
+				slideInLeft: {
+				  '0%': { transform: 'translateX(-100%)', opacity: '0' },
+				  '100%': { transform: 'translateX(0)', opacity: '1' },
+				},
+				slideInRight: {
+				  '0%': { transform: 'translateX(100%)', opacity: '0' },
+				  '100%': { transform: 'translateX(0)', opacity: '1' },
+				},
+			  },
+			  animation: {
+				slideInLeft: 'slideInLeft 1s forwards',
+				slideInRight: 'slideInRight 1s forwards',
+			},
 		}
 	},
 	plugins: [],
