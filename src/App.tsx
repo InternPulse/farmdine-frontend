dbef9654bb3db24cd2eaf64dedee0c4d561c203b
acubeos <80309463+acubeos@users.noreply.github.com>
2024-06-14 16:28:08 +0100

    Form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import Footer from "./components/Footer"
-import HomeSection from "./components/HomeSection"
-import Navbar from "./components/Navbar"
-import ProductSection from "./components/ProductSection"
-import Subscribe from "./components/Subscribe"
-import Events from "./components/Events"
+// import Footer from "./components/Footer"
+// import HomeSection from "./components/HomeSection"
+// import Navbar from "./components/Navbar"
+// import ProductSection from "./components/ProductSection"
+// import Subscribe from "./components/Subscribe"
+// import Events from "./components/Events"
+import SignUpPage from "./pages/SignUpPage"
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<HomeSection />
-			<ProductSection />
-			<Events />
-			<Subscribe />
-			<Footer />
+			<SignUpPage />
 		</>
 	)
 }
---
+++ src/customTypings/react-country-state-city.d.ts
@@ -0,0 +1,4 @@
+declare module "react-country-state-city" {
+	const noTypesYet: any
+	export default noTypesYet
+}
