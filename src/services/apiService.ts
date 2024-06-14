02500bf063cabf06aeccd4126e8bac783f7315ee
acubeos <80309463+acubeos@users.noreply.github.com>
2024-06-15 00:18:18 +0100

    Revert "Int 95/feat/product section"

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'https://farmdine-backend.onrender.com/api/v1';
+const BASE_URL = 'https://farmdine-backend.onrender.com';
 
 export const login = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/accounts/login`, {
+    const response = await axios.post(`${BASE_URL}/accounts/login`, {
       email,
       password,
     });
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-
-interface ImportMetaEnv {
-  VITE_APP_TITLE: string;
-  VITE_APP_VERSION: string;
-  REACT_APP_API_BASE_URL: string;
-  // Add other environment variables here
-}
+})
