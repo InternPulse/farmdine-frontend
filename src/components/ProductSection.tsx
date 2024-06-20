ef4e3e4d2b4b5132107e7c0f21ea4d40796f94d3
Anezijane <ahnnieanezi@gmail.com>
2024-06-20 11:37:11 +0100

    create product card component

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,5 +1,4 @@
-import Star from '../assets/Star.png'
-import Carbbage from '../assets/images/cabbage.jpg'
+import ProductCard from "./ProductCard";
 
 const ProductSection: React.FC = () => {
 
@@ -13,26 +12,7 @@ const ProductSection: React.FC = () => {
             Providing the operational support your food business needs
           </p>
         </div>
-        <div className='w-full py-4'>
-          <div className="p-4 w-1/4 bg-white shadow-md rounded-md">
-            <p className='bg-lime-100 inline-block py-1 px-2 rounded-lg text-sm ml-48'>20% off</p>
-            <img src={Carbbage} className='w-50 h-50' alt="fruit/vegetable image" />
-            <div>
-              <p>Cabbage</p>
-              <div className='flex'>
-                <p>$50.00</p>
-                <p className="text-slate-500 line-through ml-2">$70.00</p>
-              </div>
-              <div className='flex'>
-                <img src={Star} alt="star icon" className='w-4 h-4'/>
-                <img src={Star} alt="star icon" className='w-4 h-4'/>
-                <img src={Star} alt="star icon" className='w-4 h-4'/>
-                <img src={Star} alt="star icon" className='w-4 h-4'/>
-                <img src={Star} alt="star icon" className='w-4 h-4'/>
-              </div>
-            </div>
-          </div>
-        </div>
+       <ProductCard />
     </div>
   );
 };
