111a232f5dd928ee578ca3e1f1180fa531df6d8d
Anezijane <ahnnieanezi@gmail.com>
2024-06-20 11:31:40 +0100

    center content

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -7,11 +7,13 @@ const ProductSection: React.FC = () => {
   return (
     
       <div className="container mx-auto relative">
-        <h1 className="inline-block rounded-full bg-lime-100 text-lime-500 font-medium text-center mt-1 px-5 py-2 text-sm">Products</h1>
-        <p className='text-xl mb-8'>
-          Providing the operational support your food business needs
-        </p>
-        <div className='w-full bg-slate-50 py-4'>
+        <div className='text-center'>
+          <h1 className=" inline-block rounded-full bg-lime-100 text-lime-500 font-medium text-center mt-1 px-5 py-2 text-sm">Products</h1>
+          <p className='text-2xl mb-10 mt-2 font-semibold'>
+            Providing the operational support your food business needs
+          </p>
+        </div>
+        <div className='w-full py-4'>
           <div className="p-4 w-1/4 bg-white shadow-md rounded-md">
             <p className='bg-lime-100 inline-block py-1 px-2 rounded-lg text-sm ml-48'>20% off</p>
             <img src={Carbbage} className='w-50 h-50' alt="fruit/vegetable image" />
